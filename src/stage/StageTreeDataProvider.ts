ae9f06b95c069d29929affaef2d54fe68d6b9c8f
ohataken <11092425+ohataken@users.noreply.github.com>
2026-02-04 14:47:05 +0900

    chore: StageTreeDataProvider#getTreeItem

--- src/stage/StageTreeDataProvider.ts
+++ src/stage/StageTreeDataProvider.ts
@@ -2,4 +2,7 @@ import * as vscode from 'vscode';
 
 export class StageTreeDataProvider implements vscode.TreeDataProvider<any> {
     constructor(private readonly roots: any[]) {}
+
+    getTreeItem(element: any): vscode.TreeItem {
+    }
 }
